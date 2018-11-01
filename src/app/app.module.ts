67612f4a9300b5d9b8907ab270e44863cad6b698
Joseph Huizar <joseph75@mail.fresnostate.edu>
2018-11-01 00:58:24 -0700

    added routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialDesignModule} from '../material-design/material-design.module';
+
+import {AppRoutes} from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent,
+    MainFooterComponent,
+    HomeComponent,
+    CoursesComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
+    AppRoutes
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.css']
+})
+export class MainHeaderComponent implements OnInit {
+
+  private firstName: string;
+  private lastName: string;
+
+  constructor(private router: Router) {
+    this.firstName = "Joseph"
+    this.lastName = "Huizar"
+  }
+
+  goToPage(path: string) {
+    console.log("from goToPage path: ", path);
+    this.router.navigate([path]);
+  }
+
+  showGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  }
+  hideGreeting() {
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'r1c2';
+  }
+
+
+  ngOnInit() {
+  }
+
+}
